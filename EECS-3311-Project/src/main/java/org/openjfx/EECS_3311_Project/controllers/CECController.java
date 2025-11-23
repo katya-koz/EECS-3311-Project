28a0f70b3b42aae03c32928bcd305af2db1b463f
MINGW64 <manojravi04@gmail.com>
2025-11-22 21:03:44 -0500

    Finish merge + add CEC admin filter and CECTools UI

--- EECS-3311-Project/src/main/java/org/openjfx/EECS_3311_Project/controllers/CECController.java
+++ EECS-3311-Project/src/main/java/org/openjfx/EECS_3311_Project/controllers/CECController.java
@@ -1,7 +1,5 @@
 package org.openjfx.EECS_3311_Project.controllers;
 
-import java.util.ArrayList;
-
 import java.util.List;
 
 import org.openjfx.EECS_3311_Project.Mediator;
@@ -11,84 +9,85 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.control.Label;
 
 public class CECController {
 
-	@FXML
-	private StackPane contentArea;
+    @FXML
+    private StackPane contentArea;
 
-	@FXML
-	private VBox adminsList;
+    @FXML
+    private VBox adminsList;
 
-	@FXML
-	public void getAdminsEditorPage() {
-		 try {
-		        FXMLLoader loader = new FXMLLoader(
-		            getClass().getResource("/fxml/cectools/cec_admins.fxml")
-		        );
+    @FXML
+    public void getAdminsEditorPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/cectools/cec_admins.fxml")
+            );
 
-		        Node adminPage = loader.load();
+            Node adminPage = loader.load();
+            AdminsPageController controller = loader.getController();
 
-		        AdminsPageController controller = loader.getController();
+            contentArea.getChildren().setAll(adminPage);
 
-		        contentArea.getChildren().setAll(adminPage);
+         
+            List<User> users = Mediator.getInstance()
+                    .getAllUsers()
+                    .stream()
+                    .filter(u -> !u.getUserType().equalsIgnoreCase("CEC"))
+                    .toList();
+            // -----------------------------------------
 
-		        // retrieve all users
-		        List<User> users = Mediator.getInstance().getAllUsers();
+            for (User u : users) {
+                HBox row = createAdminRow(u);
+                controller.adminsList.getChildren().add(row);
+            }
 
-		        for (User u : users) {
-		            HBox row = createAdminRow(u);
-		            controller.adminsList.getChildren().add(row);
-		        }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
-	}
+    private HBox createAdminRow(User user) {
 
-	private HBox createAdminRow(User user) {
+        HBox row = new HBox(20);
+        row.setStyle("-fx-padding: 5; -fx-alignment: center-left;");
 
-		HBox row = new HBox(20);
-		row.setStyle("-fx-padding: 5; -fx-alignment: center-left;");
+        Label emailLabel = new Label(user.getEmail());
+        emailLabel.setPrefWidth(250);
 
-		Label emailLabel = new Label(user.getEmail());
-		emailLabel.setPrefWidth(250);
+        boolean isAdmin = user.getUserType().equalsIgnoreCase("Admin");
 
-		boolean isAdmin = user.getUserType().equalsIgnoreCase("Admin");
+        CheckBox adminCheck = new CheckBox("Admin?");
+        adminCheck.setSelected(isAdmin);
 
-		CheckBox adminCheck = new CheckBox("Admin?");
-		adminCheck.setSelected(isAdmin);
+        adminCheck.setOnAction(e -> {
+            boolean newValue = adminCheck.isSelected();
 
-		adminCheck.setOnAction(e -> {
-			boolean newValue = adminCheck.isSelected();
+            Mediator.getInstance().getUserManager().toggleAdmin(user.getId(), newValue);
+            user.setUserType(newValue ? "Admin" : "User");
+        });
 
-			Mediator.getInstance().getUserManager().toggleAdmin(user.getId(), newValue);
+        row.getChildren().addAll(emailLabel, adminCheck);
+        return row;
+    }
 
-		    user.setUserType(newValue ? "Admin" : "User");
-		});
+    @FXML
+    public void getRolesEditorPage() {
+        try {
+            FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("/fxml/cectools/cec_roles.fxml")
+            );
 
-		row.getChildren().addAll(emailLabel, adminCheck);
-		return row;
-	}
+            Node page = loader.load();
+            contentArea.getChildren().setAll(page);
 
-	@FXML
-	public void getRolesEditorPage() {
-		  try {
-		        FXMLLoader loader = new FXMLLoader(
-		            getClass().getResource("/fxml/cectools/cec_roles.fxml")
-		        );
-
-		        Node page = loader.load();
-		        contentArea.getChildren().setAll(page);
-
-		    } catch (Exception e) {
-		        e.printStackTrace();
-		    }
-	}
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
-
